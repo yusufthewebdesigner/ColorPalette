2a0ac4024a2704d60da3bb928929809eed419546
Yusuf <iamrealyusuf@gmail.com>
2025-04-24 11:26:44 +0600

    bug fixed

--- script.js
+++ script.js
@@ -57,49 +57,49 @@ colorCode.forEach((eachCode) => {
   let pInChildBox = document.createElement("p");
   pInChildBox.innerText = eachCode;
   childBox.append(pInChildBox);
+});
 
-  let childs = document.querySelectorAll(".childBox");
-  childs.forEach((child) => {
-    let bgColor = child.innerText;
-    child.style.background = bgColor;
-    child.addEventListener("click", () => {
-      // Play sound
-      const sound = new Howl({
-        src: ["./assets/Click-1.mp3"],
-        volume: 0.8,
-      });
-      sound.play();
-      // Copying
-      navigator.clipboard.writeText(bgColor);
-      // Showing copy msg
-      let msgBox = document.createElement("span");
-      msgBox.innerHTML = 'Copied <i class="fa-solid fa-circle-check"></i>';
-      child.prepend(msgBox);
+let childs = document.querySelectorAll(".childBox");
+childs.forEach((child) => {
+  let bgColor = child.innerText;
+  child.style.background = bgColor;
+  child.addEventListener("click", () => {
+    // Play sound
+    const sound = new Howl({
+      src: ["./assets/Click-1.mp3"],
+      volume: 0.8,
+    });
+    sound.play();
+    // Copying
+    navigator.clipboard.writeText(bgColor);
+    // Showing copy msg
+    let msgBox = document.createElement("span");
+    msgBox.innerHTML = 'Copied <i class="fa-solid fa-circle-check"></i>';
+    child.prepend(msgBox);
 
-      setTimeout(() => {
-        msgBox.remove();
-      }, 400);
-    });
+    setTimeout(() => {
+      msgBox.remove();
+    }, 400);
+  });
 
-    let hovMsg = document.createElement("span");
-    hovMsg.setAttribute("class", "hovMsg");
-    hovMsg.innerText = "Copy";
-    child.append(hovMsg);
+  let hovMsg = document.createElement("span");
+  hovMsg.setAttribute("class", "hovMsg");
+  hovMsg.innerText = "Copy";
+  child.append(hovMsg);
 
-    // to show the copy message on hover
-    child.addEventListener("mouseover", () => {
-      let hovMessage = child.querySelector(".hovMsg");
-      hovMessage.style.display = "block";
-    });
-    // to hide the copy message after clicking
-    child.addEventListener("click", () => {
-      let hovMessage = child.querySelector(".hovMsg");
-      hovMessage.style.display = "none";
-    });
-    // to hide the copy message after hover
-    child.addEventListener("mouseleave", () => {
-      let hovMessage = child.querySelector(".hovMsg");
-      hovMessage.style.display = "none";
-    });
+  // to show the copy message on hover
+  child.addEventListener("mouseover", () => {
+    let hovMessage = child.querySelector(".hovMsg");
+    hovMessage.style.display = "block";
+  });
+  // to hide the copy message after clicking
+  child.addEventListener("click", () => {
+    let hovMessage = child.querySelector(".hovMsg");
+    hovMessage.style.display = "none";
+  });
+  // to hide the copy message after hover
+  child.addEventListener("mouseleave", () => {
+    let hovMessage = child.querySelector(".hovMsg");
+    hovMessage.style.display = "none";
   });
 });
